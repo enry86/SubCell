e89e5d0106c2de0291224e1d1ee34f6f400875c9
Enrico Sartori <sartori.enrico@gmail.com>
2010-01-19 23:09:57 +0100

    dump of string kernels, continue client implementation

--- subcell.py
+++ subcell.py
@@ -12,8 +12,19 @@ Options:
     -o output file
 '''
 
+import skernel
+import os
 import getopt
 import sys
+from svm import *
+
+class Model:
+    def __init__(self, filename):
+        self.model = svm_model(filename)
+
+    def classify(self, name, vec):
+        pred = self.model.predict_probability(vec)
+        return pred
 
 
 def read_opts(argv):
@@ -21,7 +32,8 @@ def read_opts(argv):
         Command-Line options interpretation
     '''
     res = {}
-    res{'out_file'} = False 
+    res['out_file'] = False 
+    res['in_file'] = False
     try:
         opts, args = getopt.gnu_getopt(argv, 'f:o:h')
     except getopt.GetoptError, err:
@@ -46,10 +58,25 @@ def read_opts(argv):
     return res
 
 
+def init_classifier(m_dir):
+    res = {}
+    for m in os.listdir(m_dir):
+        res[m] = Model(m_dir + '/' + m)
+    return res
+
+
+def init_kernels(m_dir):
+    res = {}
+    for k in os.listdir(m_dir):
+        f = open(m_dir + '/' + k, 'r')
+        tmp[k] = pickle.load(f)
+        f.close()
+    return res
+
 def main():
     conf = read_opts(sys.argv)
-
-
+    svms = init_classifier(conf['m'])
+    krns = init_kernels(conf['m'])
 
 
 
